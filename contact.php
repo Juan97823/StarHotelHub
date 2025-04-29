6bd7c477a4073b2d1ab0839d29b10176c53282ad
juan97823 <juanesab423@gmail.com>
2025-04-29 16:01:59 -0500

    Actualización automática

--- contact.php
+++ contact.php
@@ -1,55 +1,52 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 <div class="container">
 
-<h1 class="title">Contact</h1>
+    <h1 class="title">Contacto</h1>
 
+    <!-- formulario de contacto -->
+    <div class="contact">
 
-<!-- form -->
-<div class="contact">
+        <div class="row">
 
+            <div class="col-sm-12">
+                <div class="map mb-4">
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.6897633195393!2d-74.057904!3d4.673932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a95cf99f385%3A0x577d39069b97d5b!2sHotel%20Estelar%20Parque%20de%20la%2093!5e0!3m2!1ses!2sco!4v1714400000000!5m2!1ses!2sco" 
+                        width="100%" 
+                        height="300" 
+                        style="border:0;" 
+                        allowfullscreen="" 
+                        loading="lazy" 
+                        referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
 
+            <div class="col-sm-6 col-sm-offset-3">
+                <div class="spacer">
+                    <h4>Escríbenos</h4>
+                    <form role="form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="Nombre completo">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" placeholder="Correo electrónico">
+                        </div>
+                        <div class="form-group">
+                            <input type="tel" class="form-control" id="phone" placeholder="Teléfono">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" placeholder="Mensaje" rows="4"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Enviar</button>
+                    </form>
+                </div>
+            </div>
 
-       <div class="row">
-       	
-       	<div class="col-sm-12">
-       	<div class="map">
-       	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9933.460884430251!2d-0.13301252240929382!3d51.50651527467666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C+UK!5e0!3m2!1sen!2snp!4v1414314152341" width="100%" height="300" frameborder="0"></iframe>	
-       	</div>
+        </div>
 
-
-		<div class="col-sm-6 col-sm-offset-3">
-			<div class="spacer">   		
-
-       		<h4>Write to us</h4>
-			<form role="form">
-			<div class="form-group">	
-			<input type="text" class="form-control" id="name" placeholder="Name">
-			</div>
-			<div class="form-group">
-			<input type="email" class="form-control" id="email" placeholder="Enter email">
-			</div>
-			<div class="form-group">
-			<input type="phone" class="form-control" id="phone" placeholder="Phone">
-			</div>
-			<div class="form-group">
-			<textarea type="email" class="form-control"  placeholder="Message" rows="4"></textarea>
-			</div>
-					
-			<button type="submit" class="btn btn-default">Send</button>
-			</form>
-			</div>
-
-
-       	</div>
-
-
-
-
-
-       </div>
-</div>
-</div>
-<!-- form -->
+    </div>
+    <!-- /formulario de contacto -->
 
 </div>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
