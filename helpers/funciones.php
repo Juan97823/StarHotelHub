725912f2e9066db4943dcb92a5bc4a92ee3970d8
Juan97823 <juanesab423@gmail.com>
2025-07-10 08:16:00 -0500

    Mi primer commit

--- helpers/funciones.php
+++ helpers/funciones.php
@@ -291,36 +291,30 @@ function generate_numbers($start, $count, $digits)
   }
   return $result;
 }
-// CAPTURAR EL NOMBRE DEL DIA
+// CAPTURAR EL NOMBRE DEL DÍA
 function get_nombre_dia($fecha)
 {
-  $fechats = strtotime($fecha); //pasamos a timestamp
+  $fechats = strtotime($fecha); // pasamos a timestamp
 
-  //lo devuelve en numero 0 domingo, 1 lunes,....
+  // devuelve número 0 (domingo) a 6 (sábado)
   switch (date('w', $fechats)) {
     case 0:
       return "Domingo";
-      break;
     case 1:
       return "Lunes";
-      break;
     case 2:
       return "Martes";
-      break;
     case 3:
-      return "Miercoles";
-      break;
+      return "Miércoles";
     case 4:
       return "Jueves";
-      break;
     case 5:
       return "Viernes";
-      break;
     case 6:
-      return "Sabado";
-      break;
+      return "Sábado";
   }
 }
+
 //BUSCAR VALOR EN UN ARRAY
 function verificar($valor, $datos = [])
 {
